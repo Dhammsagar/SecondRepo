b2f9619925a460b1862e88b7ff0914da2057b648
Dhammasagar Punwatkar <punwatkardhammasagar@gmail.com>
2026-04-13 23:42:22 +0530

    task1 devloper

---
+++ Project1/src/main/java/com/nit/project/Opration/Opration.java
@@ -0,0 +1,11 @@
+package com.nit.project.Opration;
+
+public class Opration {
+	
+	public int sum(int x, int y) {
+		return x+y;
+	}
+	public int sub(int x,int y) {
+		return x-y;
+	}
+}
--- Project1/src/main/java/com/nit/project/Project1/App.java
+++ Project1/src/main/java/com/nit/project/Project1/App.java
@@ -1,15 +1,16 @@
 package com.nit.project.Project1;
 
+import com.nit.project.Opration.Opration;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        
-        System.out.println(); 
-        }
+public class App {
+	public static void main(String[] args) {
+		Opration op = new Opration();
+		System.out.println("Sub::" + op.sub(10, 20));
+		System.out.println("Sum::" + op.sum(10, 30));
+
+	}
 }
