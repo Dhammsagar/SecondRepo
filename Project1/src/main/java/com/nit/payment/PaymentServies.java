1c5921155e673e77b2bd9d3cc34c539b82a01f37
Dhammasagar Punwatkar <punwatkardhammasagar@gmail.com>
2026-04-16 19:54:52 +0530

    Dev -1 task assement-1

--- Project1/src/main/java/com/nit/payment/PaymentServies.java
+++ Project1/src/main/java/com/nit/payment/PaymentServies.java
@@ -1,5 +1,11 @@
 package com.nit.payment;
 
 public class PaymentServies {
+	public String debitCard() {
+		return "DebitCard Opration..";
+	}
 
+	public String creaditCart() {
+		return "Creadit Opration";
+	}
 }
